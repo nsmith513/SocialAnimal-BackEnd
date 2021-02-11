b8466f89afd7bd800f8081ccc0566c80c595b145
Nicholas Smith <nicholas.jd.smith5@gmail.com>
2021-02-11 12:14:39 -0800

    reworked per image model for links instead of byte arrays

--- SocialAnimal-BackEnd/src/main/java/com/revature/models/PetImage.java
+++ SocialAnimal-BackEnd/src/main/java/com/revature/models/PetImage.java
@@ -1,5 +1,6 @@
 package com.revature.models;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -12,9 +13,49 @@ import lombok.Data;
 public class PetImage {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int pet_id;
+    private int id;
     
-    private byte[] petImage;
-    
+    @Column(name="link", unique=true, nullable=false)
+    private String link;
 
+	public PetImage() {}
+
+	public PetImage(String link) {
+		super();
+		this.link = link;
+	}
+
+	public PetImage(int id, String link) {
+		super();
+		this.id = id;
+		this.link = link;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + id;
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		PetImage other = (PetImage) obj;
+		if (id != other.id)
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "PetImage [id=" + id + ", link=" + link + "]";
+	}
+    
 }
